2d89ab92f1deb5b85677888bfb45c068d6592062
Tofayel Ahmed Topu <53596291+killswitch99@users.noreply.github.com>
2021-09-20 23:52:29 +1000

    paymentMethod

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -30,8 +30,16 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
 const userInfoFromStrorage = localStorage.getItem('userInfo')
 	? JSON.parse(localStorage.getItem('userInfo'))
 	: null
+
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+	? JSON.parse(localStorage.getItem('shippingAddress'))
+	: {}
+
 const initialState = {
-	cart: { cartItems: cartItemsFromStorage },
+	cart: {
+		cartItems: cartItemsFromStorage,
+		shippingAddress: shippingAddressFromStorage,
+	},
 	userLogin: { userInfo: userInfoFromStrorage },
 }
 const middleware = [thunk]
